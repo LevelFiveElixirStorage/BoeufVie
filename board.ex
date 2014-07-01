6f653e1bc156e64e681f5aec6595b52c84ebe15d
Dean <deanfreekinggalvin@gmail.com>
2014-07-01 12:16:37 -0400

    inital board push

---
+++ board.ex
@@ -0,0 +1,14 @@
+# First we need to create a board
+defmodule Board do
+  board = ()
+
+  def new(size) do
+  end
+end
+
+def module Build do
+  def createNew(board, curSize, Size) when curSize == prevSize do
+    board
+  end
+
+  def createNew(board, curSize, prevSize) when
