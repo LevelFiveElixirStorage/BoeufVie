d7339df2ef990e821bd903288a51a7680931dd85
Dean <deanfreekinggalvin@gmail.com>
2014-07-01 12:43:26 -0400

    fixed compile errors

--- board.ex
+++ board.ex
@@ -1,14 +1,18 @@
 # First we need to create a board
 defmodule Board do
-  board = {}
-
   def new(size) do
-    Build.createNew(board, 0, goalSize)
+    Build.create_new({}, 0, size)
   end
 end
 
 defmodule Build do
   def create_new(board, size, goalSize) when size <= goalSize do
-    board = Tuple.insert_at(board, 0, Tupple.duplicate(:dead, goalSize))
+    board = Tuple.insert_at(board, 0, Tuple.duplicate(:dead, goalSize))
+    size = size + 1
+    create_new(board, size, goalSize)
+  end
+
+  def create_new(board, size, goalSize) when size > goalSize do
+    board
   end
 end
