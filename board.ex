8b50b5566ee0600b50af0c9e3fa1a86d0ca48eb8
Caleb Everett <everett.caleb@gmail.com>
2014-07-01 12:56:00 -0400

    Add cell and adj_cells functions

--- board.ex
+++ board.ex
@@ -1,14 +1,25 @@
 # First we need to create a board
 defmodule Board do
-  board = ()
 
   def new(size) do
   end
-end
 
-def module Build do
-  def createNew(board, curSize, Size) when curSize == prevSize do
-    board
+  # Returns the value of the passed cell.
+  def cell(board, {x, y}) do
+    elem(elem(board, x), y)
   end
 
-  def createNew(board, curSize, prevSize) when
+  # Returns list of the values of surounding cells.
+  def adj_cells(board, {x, y}) do
+    x_dim = tuple_size(board)
+    y_dim = tuple_size(elem(board, 1))
+
+    for x_p <- (x-1)..(x+1),       # Three rows around this cell.
+        y_p <- (y-1)..(y+1),       # Three columns around this cell.
+        x_p != x || y_p != y,      # Not the passed cell.
+        x_p >= 0 && x_p < x_dim,   # X bounds check.
+        y_p >= 0 && y_p < y_dim do # Y bounds check.
+          cell(board, {x_p, y_p})
+        end
+  end
+end
